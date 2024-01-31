4016e839792661686fdbb93bd1c59f220e9e08b5
Rithik.Modi <Rithik.Modi@PT190LT>
2024-01-31 11:45:04 +0530

    Add project files.

---
+++ Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResourceManagement.UI.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+
+        [Required]
+        [DisplayName("Role")]
+        public string RoleName { get; set; } = string.Empty;
+        public Employee? Employee { get; set; }
+        public List<ProjectRole>? ProjectRoles { get; }
+        public EmployeeProject? EmployeeProject { get; set; }
+
+    }
+}
---
+++ ViewModels/StatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ResourceManagement.UI.ViewModels
+{
+    public class StatusViewModel
+    {
+        public List<SelectListItem> Status { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "On Going", Text = "On Going" },
+            new SelectListItem { Value = "Completed", Text = "Completed" },
+            new SelectListItem { Value = "Future", Text = "Future"  },
+        };
+    }
+}
